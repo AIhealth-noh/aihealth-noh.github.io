512e5f030ca11e078d0585435468905a1623ec77
AIhealth-noh <150212956+AIhealth-noh@users.noreply.github.com>
2026-05-10 00:56:19 +0000

    Deploying to gh-pages from @ AIhealth-noh/aihealth-noh.github.io@881e11c9c8988cc7e117e6048f211ed1d413502c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-selected-as-pi-for-the-ministry-of-health-and-welfare-mohw-project-on-ai-based-rare-liver-disease-diagnosis",
+          title: 'Selected as PI for the Ministry of Health and Welfare (MOHW) project on...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
