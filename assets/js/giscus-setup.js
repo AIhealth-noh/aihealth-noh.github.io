5294052ab0da7689ee58cf702ee0f814078b9d3c
AIhealth-noh <150212956+AIhealth-noh@users.noreply.github.com>
2026-05-10 00:31:12 +0000

    Deploying to gh-pages from @ AIhealth-noh/aihealth-noh.github.io@5ce619345c1ce95107c413f5352adcebc8c734d8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AIhealth-noh/Alhealth-noh.github.io",
+    "data-repo": "AIhealth-noh/aihealth-noh.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
